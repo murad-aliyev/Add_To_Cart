4995f3b49db1a45a74e5b1df3910d7107b2ebc89
Murad R. Aliyev <muradrali@code.local>
2023-07-27 17:38:36 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const productsData = [
+    { id: 1, title: "Flower Title 1", image: "./images/flower.jpg", desc: "This is some description about yhe product" },
+    { id: 2, title: "Flower Title 2", image: "./images/house.jpeg", desc: "This is some description about yhe product" },
+    { id: 3, title: "Flower Title 3", image: "./images/car.jpg", desc: "This is some description about yhe product" },
+    { id: 4, title: "Flower Title 4", image: "./images/flower.jpg", desc: "This is some description about yhe product" },
+    { id: 5, title: "Flower Title 5", image: "./images/house.jpeg", desc: "This is some description about yhe product" },
+    { id: 6, title: "Flower Title 6", image: "./images/car.jpg", desc: "This is some description about yhe product" },
+    { id: 7, title: "Flower Title 7", image: "./images/flower.jpg", desc: "This is some description about yhe product" },
+    { id: 8, title: "Flower Title 8", image: "./images/house.jpeg", desc: "This is some description about yhe product" },
+    { id: 9, title: "Flower Title 9", image: "./images/car.jpg", desc: "This is some description about yhe product" },
+    { id: 10, title: "Flower Title 10", image: "./images/flower.jpg", desc: "This is some description about yhe product" }
+]
